be44d0f1cc7203485971558a326433967e36bb6d
tosimb <tosimb@yandex.ru>
2026-05-27 14:57:59 +0300

    Add 1-vehicles

---
+++ 1-vehicles/task_1.sql
@@ -0,0 +1,9 @@
+SELECT
+    v.maker,
+    v.model
+FROM Vehicle v
+JOIN Motorcycle m ON v.model = m.model
+WHERE m.horsepower > 150
+  AND m.price < 20000
+  AND m.type = 'Sport'
+ORDER BY m.horsepower DESC;
